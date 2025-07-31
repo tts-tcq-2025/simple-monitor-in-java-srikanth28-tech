d067e44b1c191e2137b7c0f3cb94cb9de1b11c3e
srikanth28-tech <srikanthmsd47@gmail.com>
2025-07-31 17:18:34 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -5,10 +5,12 @@ public class Main {
         if(temperature < 0 || temperature > 45) {
             System.out.println("Temperature is out of range!");
             return false;
-        } else if(soc < 20 || soc > 80) {
+        } 
+        if(soc < 20 || soc > 80) {
             System.out.println("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        }
+        if(chargeRate > 0.8) {
             System.out.println("Charge Rate is out of range!");
             return false;
         }
